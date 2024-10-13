4bed3de8fc712af67964ccd1a952b21b0e2496d4
Eliel Silva <elielmsilvatec@gmail.com>
2024-10-13 17:27:27 -0400

    teste 2

--- src/app/components/DeleteConfirmation/page.tsx
+++ src/app/components/DeleteConfirmation/page.tsx
@@ -8,7 +8,7 @@ interface DeleteConfirmationProps {
   onConfirm: () => void;
 }
 
-const DeleteConfirmation = ({ onConfirm }: DeleteConfirmationProps) => {
+const DeleteConfirmation: React.FC<DeleteConfirmationProps> = ({ onConfirm }) => {
   const handleDelete = () => {
     MySwal.fire({
       title: 'Você tem certeza?',
@@ -18,13 +18,13 @@ const DeleteConfirmation = ({ onConfirm }: DeleteConfirmationProps) => {
       confirmButtonColor: '#006400',
       cancelButtonColor: '#d33',
       confirmButtonText: 'Sim, continuar!',
-      cancelButtonText: 'Não, cancelar!'
+      cancelButtonText: 'Não, cancelar!',
     }).then((result) => {
       if (result.isConfirmed) {
         MySwal.fire({
           title: 'Excluído!',
           text: 'Seu arquivo foi excluído.',
-          icon: 'success'
+          icon: 'success',
         });
         if (onConfirm) {
           onConfirm();
@@ -40,53 +40,4 @@ const DeleteConfirmation = ({ onConfirm }: DeleteConfirmationProps) => {
   );
 };
 
-export default DeleteConfirmation
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import Swal from 'sweetalert2';
-// import withReactContent from 'sweetalert2-react-content';
-
-// const MySwal = withReactContent(Swal);
-
-// const DeleteConfirmation = ({ onConfirm }) => {
-//   const handleDelete = () => {
-//     MySwal.fire({
-//       title: 'Você tem certeza?',
-//       text: "Você não poderá reverter isso!",
-//       icon: 'warning',
-//       showCancelButton: true,
-//       confirmButtonColor: '#006400',
-//       cancelButtonColor: '#d33',
-//       confirmButtonText: 'Sim, continuar!',
-//       cancelButtonText: 'Não, cancelar!'
-//     }).then((result) => {
-//       if (result.isConfirmed) {
-//         MySwal.fire({
-//           title: 'Excluído!',
-//           text: 'Seu arquivo foi excluído.',
-//           icon: 'success'
-//         });
-//         if (onConfirm) {
-//           onConfirm();
-//         }
-//       }
-//     });
-//   };
-
-//   return (
-//     <button onClick={handleDelete} className="btn btn-danger">
-//       Excluir
-//     </button>
-//   );
-// };
-
-// export default DeleteConfirmation;
+export default DeleteConfirmation;
