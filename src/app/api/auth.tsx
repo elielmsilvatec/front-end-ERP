a5e2bb6f68cca71f31fc76f19d43219f3c256985
Eliel Silva <elielmsilvatec@gmail.com>
2024-10-19 21:58:58 -0400

    teste 17

--- src/app/api/auth.tsx
+++ src/app/api/auth.tsx
@@ -1,12 +1,11 @@
 // utils/auth.js
-
 'use client';
 
 import api from "@/app/api/api";
-import { useRouter } from "next/navigation"; 
+import { useRouter } from "next/navigation";
 import { useEffect, useState, useCallback } from "react";
 
-export const useAuth = () => {
+export function useAuth() {
   const [user, setUser] = useState(null);
   const [loggedIn, setLoggedIn] = useState(false);
   const router = useRouter();
@@ -15,14 +14,15 @@ export const useAuth = () => {
     if (typeof window === 'undefined') {
       return; // Se estamos no servidor, não faz nada
     }
-    // http://localhost:5000/session-info
+
     try {
       const response = await api.get("https://data.colorado.gov/resource", {
         withCredentials: true,
       });
+
       if (response.data.loggedIn) {
         setUser(response.data.user);
-        console.log(response.data.user)
+        console.log(response.data.user);
         setLoggedIn(true);
       } else {
         router.push("/pages/login/login");
@@ -38,9 +38,53 @@ export const useAuth = () => {
   }, [fetchSessionInfo]);
 
   return { user, loggedIn };
-};
+}
 
 
+
+
+
+
+
+// 'use client';
+
+// import api from "@/app/api/api";
+// import { useRouter } from "next/navigation"; 
+// import { useEffect, useState, useCallback } from "react";
+
+// export const useAuth = () => {
+//   const [user, setUser] = useState(null);
+//   const [loggedIn, setLoggedIn] = useState(false);
+//   const router = useRouter();
+
+//   const fetchSessionInfo = useCallback(async () => {
+//     if (typeof window === 'undefined') {
+//       return; // Se estamos no servidor, não faz nada
+//     }
+//     // http://localhost:5000/session-info
+//     try {
+//       const response = await api.get("https://data.colorado.gov/resource", {
+//         withCredentials: true,
+//       });
+//       if (response.data.loggedIn) {
+//         setUser(response.data.user);
+//         console.log(response.data.user)
+//         setLoggedIn(true);
+//       } else {
+//         router.push("/pages/login/login");
+//       }
+//     } catch (error) {
+//       console.error("Erro ao buscar informações da sessão:", error);
+//       router.push("/pages/login/login");
+//     }
+//   }, [router]);
+
+//   useEffect(() => {
+//     fetchSessionInfo();
+//   }, [fetchSessionInfo]);
+
+//   return { user, loggedIn };
+// };
 
 
 
