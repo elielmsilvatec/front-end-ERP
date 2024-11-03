ac9298414c53037501d7d15b6e96e3c1c53abc02
Eliel Silva <elielmsilvatec@gmail.com>
2024-11-03 11:35:58 -0400

    asdasdasd

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -3,17 +3,12 @@
 // Inspired by react-hot-toast library
 import * as React from 'react';
 
-import type { ToastActionElement, ToastProps } from '@/components/ui/toast';
+
 
 const TOAST_LIMIT = 1;
 const TOAST_REMOVE_DELAY = 1000000;
 
-type ToasterToast = ToastProps & {
-  id: string;
-  title?: React.ReactNode;
-  description?: React.ReactNode;
-  action?: ToastActionElement;
-};
+
 
 const actionTypes = {
   ADD_TOAST: 'ADD_TOAST',
